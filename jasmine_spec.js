6c2a07d61209910f7eef90b3ae5ac40bd7878652
Chris Geihsler <cgeihsler@thinkthroughmath.com>
2013-02-13 18:45:45 -0500

    Added javascript testing framework: jasmine-rails

---
+++ jasmine_spec.js
@@ -0,0 +1,10 @@
+describe("Jasmine", function() {
+  var a;
+
+  it("should work", function() {
+    //calling this function from classroom.js, to prove that it's included
+    removeSelectedTeacher();
+
+    expect(true).toBe(true);
+  });
+});
