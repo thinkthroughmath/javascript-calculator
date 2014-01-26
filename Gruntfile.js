d73884cfa384dcee7be04362819c472edc8278e1
Joel McCracken <mccracken.joel@gmail.com>
2014-01-26 10:41:28 -0500

    serve now on 0.0.0.0:10000

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,9 +51,9 @@ module.exports = function (grunt) {
 
     connect: {
       options: {
-        port: 9001,
+        port: 10000,
         // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       serve: {
         options: {
